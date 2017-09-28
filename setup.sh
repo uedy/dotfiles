f564061de939e86aa3b60eff7c0fa93a33a8efac
上田　直樹 <naoki.ueda.612@gmail.com>
2017-09-29 08:58:14 +0900

    余分な者の駆除

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@
 ln -sf ~/dotfiles/_vim/_vimrc ~/.vimrc
 ln -sf ~/dotfiles/_vim/_gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/_vim ~/.vim
+ln -sf ~/dotfiles/zsh/_zprofile ~/.zprofile
+ln -sf ~/dotfiles/zsh/_zshrc ~/.zshrc
+
